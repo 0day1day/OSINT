e0c2dbddda4b5a128d1717168db0e52cac8993a3
AlienOne <ali3n0ne@alienone.org>
2013-03-01 15:01:28 -0500

    bogon test

--- TESTING-REFACTOR-REWRITE/VirusTotal/Test/C2/IP/cymru_bogon_lookup.py
+++ TESTING-REFACTOR-REWRITE/VirusTotal/Test/C2/IP/cymru_bogon_lookup.py
@@ -1,9 +1,23 @@
+import re
 from cymru.bogon.dns import DNSClient as bogon
+
+regex_dict = {"Email": '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$',
+              "Phone": '^(?:\+?1[-. ]?)?\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$',
+              "IPAddress": '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'}
+RegexFactory = type('RegexFactory', (), regex_dict)
+obj = RegexFactory()
+
+
+def regexFactory(object_name, twitter_tweet):
+    search_parameter = re.compile(r'%s' % object_name, re.IGNORECASE)
+    return search_parameter.findall(twitter_tweet)
 
 
 def bogon_check(ip_address):
     client = bogon()
-    return client.lookupmany_dict(ip_address, 'IP')
+    return_dict = client.lookupmany_dict(ip_address, 'IP')
+    return return_dict.values()[0]
 
-ip_address = ['10.10.10.10']
-print bogon_check(ip_address)
+tweet_en = "this is tweet with an IP Address in the tweet. 0.0.0.0"
+cull_ipaddress = regexFactory(obj.IPAddress, tweet_en)
+print(bogon_check(cull_ipaddress))
